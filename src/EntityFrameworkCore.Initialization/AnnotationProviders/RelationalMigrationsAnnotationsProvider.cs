31cb03354e0db081ef24cf953c82b90bf8465827
David Ikin <davidikin45@gmail.com>
2020-11-20 18:11:44 +0000

    changes

---
+++ src/EntityFrameworkCore.Initialization/AnnotationProviders/RelationalMigrationsAnnotationsProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityFrameworkCore.Initialization.AnnotationProviders
+{
+    //public class RelationalMigrationsAnnotationsProvider : SqlServerMigrationsAnnotationProvider
+    //{
+    //    private readonly SqliteMigrationsAnnotationProvider _sqliteMigrationsAnnotationProvider;
+
+    //    public RelationalMigrationsAnnotationsProvider(MigrationsAnnotationProviderDependencies dependencies)
+    //       : base(dependencies)
+    //    {
+    //        _sqliteMigrationsAnnotationProvider = new SqliteMigrationsAnnotationProvider(dependencies);
+    //    }
+
+    //    public override IEnumerable<IAnnotation> For(IModel model)
+    //    {
+    //        return base.For(model).Concat(_sqliteMigrationsAnnotationProvider.For(model));
+    //    }
+
+    //    public override IEnumerable<IAnnotation> For(IProperty property)
+    //    {
+    //        return base.For(property).Concat(_sqliteMigrationsAnnotationProvider.For(property));
+    //    }
+    //}
+}
